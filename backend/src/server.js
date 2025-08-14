a0d420b11410c2f46a5bbe9dda033a785a89bd1d
ditsus <170345142+ditsus@users.noreply.github.com>
2025-08-14 14:22:00 -0400

    Deployment prep

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import rateLimiter from "./middleware/rateLimiter.js";
 import notesRoutes from "./routes/notesRoutes.js";
@@ -8,14 +9,17 @@ import { connectDB } from "./config/db.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 const app = express();
 
 // middleware
-//cors is default in every browser. it sees how the api is called from a different source, so it blocks it. app.use(cors()) allows usage from any source
-app.use(cors({
-    origin:"http://localhost:5173",
-}));
+if (process.env.NODE_ENV !== "production") {
+    //cors is default in every browser. it sees how the api is called from a different source, so it blocks it. app.use(cors()) allows usage from any source
+    app.use(cors({
+        origin:"http://localhost:5173",
+    }));
+}
 app.use(express.json()); // allows parsing of middleware bodies, allowing access to req.body
 app.use(rateLimiter);
 
@@ -28,6 +32,15 @@ app.use(rateLimiter);
 // })
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"));
+    });
+}
+
 
 // connect to mongoDB first, then listen to PORT
 connectDB().then(() => {
